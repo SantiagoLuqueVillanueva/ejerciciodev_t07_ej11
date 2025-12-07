a4816cb2db735f53445d75a9aa04e46118441d75
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-12-07 13:09:25 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,60 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] origin = new int[10];
+
+        System.out.println("Introduzca 10 números separados por INTRO:");
+
+        for (int i = 0; i < origin.length; i++) {
+            origin[i] = Integer.parseInt(System.console().readLine());
+        }
+
+        System.out.println();
+        System.out.println("Array original:");
+
+        int[] indice = new int[10];
+        int[] destino = new int[10];
+
+        for (int i = 0; i < indice.length; i++) {
+            System.out.print((indice[i] = i)+" ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < indice.length; i++) {
+            System.out.print(origin[i] + " ");
+        }
+
+        System.out.println();
+        int contador = 0;
+
+        for (int i = 0; i < origin.length; i++) {
+            if (esPrimo(origin[i])) {
+                destino[contador] = origin[i];
+                contador++;
+            }
+        }
+
+        for (int i = 0; i < origin.length; i++) {
+            if (!esPrimo(origin[i])) {
+                destino[contador] = origin[i];
+                contador++;
+            }
+        }
+
+        for (int i = 0; i < destino.length; i++) {
+            System.out.print(destino[i] + " ");
+        }
+    }
+
+    private static boolean esPrimo(int num){
+        if (num <= 1)
+            return false;
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) 
+                return false;
+        }
+
+        return true;
     }
 }
